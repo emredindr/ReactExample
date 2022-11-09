ee95da3dc9366ab1b7bcc9ec16adf86f86bc036d
Emre DİNDAR <75311655+emredindr@users.noreply.github.com>
2022-11-09 17:33:23 +0300

    hello-react is created

---
+++ hello-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header'
+function App() {
+  return (
+    <div className="App">
+      <h1>Emre</h1>
+      <Header></Header>
+      <p>Loremm</p>
+    </div>
+  );
+}
+
+export default App;
